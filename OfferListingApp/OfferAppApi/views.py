4f212eb729cb8acb082acfb8453800bdf504ed31
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2019-07-11 13:02:28 +0530

    Add views

--- OfferListingApp/OfferAppApi/views.py
+++ OfferListingApp/OfferAppApi/views.py
@@ -1,3 +1,79 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Location, Store, Offer, User, Order, Payment, PaymentType
+from . serializers import UserSerializer, LocationSerializer, StoreSerializer, OfferSerializer
+from . serializers import PaymentSerializer, PaymentTypeSerializer, OrderSerializer
 
-# Create your views here.
+class UserList(APIView):
+
+    def get(self, request):
+        user = User.objects.all()
+        serializer = UserSerializer(user, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
+    
+class LocationList(APIView):
+
+    def get(self, request):
+        location = Location.objects.all()
+        serializer = LocationSerializer(location, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
+
+class OfferList(APIView):
+
+    def get(self, request):
+        offer = Offer.objects.all()
+        serializer = OfferSerializer(offer, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
+
+class StoreList(APIView):
+
+    def get(self, request):
+        store = Store.objects.all()
+        serializer = StoreSerializer(store, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
+    
+class PaymentList(APIView):
+
+    def get(self, request):
+        payment = Payment.objects.all()
+        serializer = PaymentSerializer(payment, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
+
+class PaymentTypeList(APIView):
+
+    def get(self, request):
+        payment_type = PaymentType.objects.all()
+        serializer = PaymentTypeSerializer(payment_type, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
+
+class OrderList(APIView):
+
+    def get(self, request):
+        order = Order.objects.all()
+        serializer = OrderSerializer(order, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
